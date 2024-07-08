2bdc880d1dcbc89f0ed127efde876bd1a1de90c3
busma13 <pluitjens@terascope.io>
2024-07-08 14:34:05 -0700

    es testing

--- esTest.js
+++ esTest.js
@@ -1,4 +1,16 @@
 import { execa } from "execa";
+import got from 'got';
 
-execa`docker run -d -p 127.0.0.1:9200:9200 -p 127.0.0.1:9300:9300 -e "discovery.type=single-node" elasticsearch:7.9.3`
+execa`docker run -d -p 9200:9200 -e "discovery.type=single-node" -e discovery.seed_hosts="localhost" elasticsearch:7.9.3`
 
+let res;
+let retry = true;
+while (retry) {
+    try {
+        res = await got.get('http://localhost:9200')
+        retry = false;
+    } catch (e) {
+        console.log('error: ', e.message)
+    }
+}
+console.log("@@@ res: ", res.body)
