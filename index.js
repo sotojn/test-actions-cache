7baf25240504c3d2239abdd33ef219bd0b68a827
busma13 <pluitjens@terascope.io>
2024-07-08 11:36:34 -0700

    await execa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { execa } from 'execa';
 images('list')
 images('load')
 
-execa`docker pull terascope/node-base:18.19.1`; 
-execa`docker pull terascope/node-base:20.11.1`; 
-execa`docker pull terascope/node-base:22.2.0`; 
+await execa`docker pull terascope/node-base:18.19.1`; 
+await execa`docker pull terascope/node-base:20.11.1`; 
+await execa`docker pull terascope/node-base:22.2.0`; 
 console.log('Test complete!');
